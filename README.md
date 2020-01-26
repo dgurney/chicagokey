32df6cc54db231a40e5019c7d7266be4336b6a9e
Daniel Gurney <dgurney99@gmail.com>
2020-01-26 14:43:26 +0200

    v0.0.1

---
+++ README.md
@@ -0,0 +1,11 @@
+# chicagokey
+Beta Site ID + Password generator for old Windows 95 (Chicago) builds.
+
+## Usage
+```
+-b string
+        Build to generate the site id and password for. Valid options: 73f, 73g, 81, 99 (for builds up to 116), 122 (for builds up to 189), 216 (for builds up to 275).
+-r int
+        Repeat n times. (default 1)
+-v    Print version information and exit.
+```
--- chicagokey.go
+++ chicagokey.go
@@ -1,5 +1,19 @@
 package main
 
+/*
+   Copyright (C) 2020 Daniel Gurney
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import (
 	"flag"
 	"fmt"
@@ -21,7 +35,7 @@ func getVersion() string {
 }
 
 func main() {
-	b := flag.String("b", "", "Build to generate the site id and password for. Valid options: 73f, 73g, 81, 99 (for builds up to 116), 122 (for builds up to 189), 216 (for builds up to 275)")
+	b := flag.String("b", "", "Build to generate the site id and password for. Valid options: 73f, 73g, 81, 99 (for builds up to 116), 122 (for builds up to 189), 216 (for builds up to 275).")
 	r := flag.Int("r", 1, "Repeat n times.")
 	v := flag.Bool("v", false, "Print version information and exit.")
 	flag.Parse()
